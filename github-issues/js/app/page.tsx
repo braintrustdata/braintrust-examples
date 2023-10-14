da6bf34a36c14e068669ff600cb6a18145bbd516
BrainTrust Engineering <>
2023-10-14 15:13:56 -0700

    Making progress

--- github-issues/js/app/page.tsx
+++ github-issues/js/app/page.tsx
@@ -4,13 +4,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex"></div>
-
       <div className="flex">
         <GithubBox />
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left"></div>
     </main>
   );
 }
