9b0d677bc6afdb2372df91da112a024c671ba9dc
BrainTrust Engineering <>
2023-10-15 14:23:31 -0700

    Improve

--- github-issues/js/evals/issue.eval.ts
+++ github-issues/js/evals/issue.eval.ts
@@ -1,13 +1,10 @@
-import { Eval, initDataset } from "braintrust";
+import { Eval, currentSpan } from "braintrust";
 import { Summary } from "autoevals";
-import OpenAI from "openai";
 
 import { titleGeneratorMessages } from "@/app/api/completion/route";
 import { loadIssues } from "./load";
+import { chatCompletion } from "./oai";
 
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-});
 const MODEL = "gpt-3.5-turbo";
 
 Eval("gh-issues", {
@@ -23,49 +20,12 @@ Eval("gh-issues", {
     });
   },
   task: async (input, { span }) => {
-    return await span.startSpanWithCallback({ name: "foo" }, async (span) => {
-      const res = await openai.chat.completions.create({
-        model: MODEL,
-        messages: titleGeneratorMessages(input),
-        temperature: 0,
-      });
-      span.log({ input: "foo" });
-      return res.choices[0].message!.content!;
+    const res = await chatCompletion(span, {
+      model: MODEL,
+      messages: titleGeneratorMessages(input),
+      temperature: 0,
     });
-    return span.startSpanWithCallback(
-      {
-        name: "OpenAI Completion",
-      },
-      async () => {
-        // XXX Wrap the OpenAI call here
-        const res = await openai.chat.completions.create({
-          model: MODEL,
-          messages: titleGeneratorMessages(input),
-          temperature: 0,
-        });
-        /*
-   input = input_args.pop("messages")
-    span.log(
-        metrics={
-            "tokens": response["usage"]["total_tokens"],
-            "prompt_tokens": response["usage"]["prompt_tokens"],
-            "completion_tokens": response["usage"]["completion_tokens"],
-        },
-        metadata={**input_args, **kwargs},
-        input=input,
-        output=response["choices"][0],
-    )
-    */
-        span.log({
-          metrics: {
-            tokens: res.usage?.total_tokens,
-            prompt_tokens: res.usage?.prompt_tokens,
-            completion_tokens: res.usage?.completion_tokens,
-          },
-        });
-        return res.choices[0].message!.content!;
-      }
-    );
+    return res.choices[0].message!.content!;
   },
   scores: [Summary],
 });
---
+++ github-issues/js/evals/oai.ts
@@ -0,0 +1,38 @@
+import { Span, currentSpan } from "braintrust";
+import OpenAI from "openai";
+import {
+  ChatCompletion,
+  ChatCompletionChunk,
+  ChatCompletionCreateParams,
+} from "openai/resources/index.mjs";
+import { Stream } from "openai/streaming.mjs";
+
+export const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+export async function chatCompletion(
+  span: Span,
+  params: Omit<ChatCompletionCreateParams, "stream">
+): Promise<ChatCompletion> {
+  return await span.startSpanWithCallback(
+    {
+      name: "OpenAI Completion",
+    },
+    async (span) => {
+      const { messages, ...rest } = params;
+      const ret = await openai.chat.completions.create(params);
+      span.log({
+        input: messages,
+        metadata: rest,
+        output: ret.choices[0],
+        metrics: {
+          tokens: ret.usage?.total_tokens,
+          prompt_tokens: ret.usage?.prompt_tokens,
+          completion_tokens: ret.usage?.completion_tokens,
+        },
+      });
+      return ret;
+    }
+  );
+}
